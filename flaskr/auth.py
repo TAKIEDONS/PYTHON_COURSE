b4fabc227668a7c67d8d1520570ecdff2933f5ee
TAKIEDONS <takalani@qrspace.io>
2022-05-20 08:48:36 +0200

    first commit

---
+++ flaskr/auth.py
@@ -0,0 +1,105 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# register view
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+               # takes a SQL query with ? placeholders for any user input, and a tuple of values 
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    # securely hash the password, and that hash is stored
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+                 # occur if the username already exists
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+# view login
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+            #returns one row from the query.
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('blog.index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+# load view session
+
+# registers a function that runs before the view function
+@bp.before_app_request
+# checks if a user id is stored in the session and gets that user’s data from the database, storing it on g.user
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+# remove the user id from the session
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("auth.login"))
+
+
+# Creating, editing, and deleting blog posts will require a user to be logged in.
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ flaskr/db.py
@@ -0,0 +1,46 @@
+# etablish connection to the database
+import sqlite3
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+    if 'db' not in g:
+# establishes a connection to the file pointed at by the DATABASE configuration key.        
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+# tells the connection to return rows that behave like dicts.        
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+# close connection 
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+# Add the Python functions that will run these SQL commands to the db.py file
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+# Register with the Application
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
